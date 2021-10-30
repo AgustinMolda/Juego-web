7051345b4411d4ce87619e20243f6d8271853a5b
Agustin Moldavsky <eberandalf@gmail.com>
2021-10-30 16:01:58 -0300

    Crear rectangulos de colores

--- js/inicio.js
+++ js/inicio.js
@@ -10,6 +10,8 @@ var inicio = {
     iniciarJuego: function(){
 
         console.log("Juego iniciado");
+        var r = new Rectangulo(0,0,100,100);
+        var r2= new Rectangulo(100,0,100,100);
         dimensiones.iniciar();
         buclePrincipal.iterar();
 
