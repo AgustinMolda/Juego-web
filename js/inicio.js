4f829fa8a879d7dad34f510b82855d739184bd9a
Agustin Moldavsky <eberandalf@gmail.com>
2021-10-30 17:16:17 -0300

    Prueba de cuadrados

--- js/inicio.js
+++ js/inicio.js
@@ -10,11 +10,18 @@ var inicio = {
     iniciarJuego: function(){
 
         console.log("Juego iniciado");
-        var r = new Rectangulo(0,0,100,100);
-        var r2= new Rectangulo(100,0,100,100);
         dimensiones.iniciar();
+        inicio.recargarTiles();
         buclePrincipal.iterar();
 
+    },
+    recargarTiles: ()=>{
+        document.querySelector("#juego").innerHTML= "";
+        for(let y = 0; y<dimensiones.obtenerTilesVerticales();y++){
+            for(let x = 0; x<dimensiones.obtenerTilesHorizontales();x++){
+                var r = new Rectangulo(x * dimensiones.ladoTiles, y * dimensiones.ladoTiles,dimensiones.ladoTiles,dimensiones.ladoTiles);
+            }
+        }
     }
 
 };
